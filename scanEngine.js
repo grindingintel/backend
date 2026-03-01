704d2781a343d367fc3a882e1d35f899852f4cc4
grindingintel <mmeyer@dsinc.pro>
2026-03-01 13:11:00 -0600

    Update scanEngine.js

--- scanEngine.js
+++ scanEngine.js
@@ -1,10 +1,10 @@
 // scanEngine.js
 import axios from 'axios';
 import { load } from 'cheerio';
-
-const $ = load(html);
 import pkg from 'pg';
+
 const { Pool } = pkg;
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
